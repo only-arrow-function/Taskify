409bbc9abcf8522b3a96ae0e35c0aa7efc6a2294
ayoung-iya <rhaehfdl1879@naver.com>
2024-04-15 23:15:29 +0900

    refactor [윤아영] : Image 태그를 div 태그로 감싸고 fill 속성 적용

--- components/chips/color-chip.tsx
+++ components/chips/color-chip.tsx
@@ -16,11 +16,9 @@ const ColorChip = ({ color, checked }: { color: Color; checked?: boolean }) => {
     <div className="relative size-[28px] md:size-[30px]">
       <div className={`size-full rounded-full ${colorConfig[color]}`}></div>
       {checked && (
-        <Image
-          src={CheckIcon}
-          alt="선택됨"
-          className="absolute top-2/4 left-2/4 translate-x-[-50%] translate-y-[-50%] size-[22px] md:size-[24px]"
-        />
+        <div className="absolute top-2/4 left-2/4 translate-x-[-50%] translate-y-[-50%] size-[22px] md:size-[24px]">
+          <Image src={CheckIcon} alt="선택됨" fill />
+        </div>
       )}
     </div>
   );
