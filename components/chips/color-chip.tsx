967568e2bfbbed5e50be8b571c19f31535c97b03
ayoung-iya <rhaehfdl1879@naver.com>
2024-04-16 18:59:34 +0900

    refactor [윤아영] : 반응형 분기점 수정

--- components/chips/color-chip.tsx
+++ components/chips/color-chip.tsx
@@ -5,12 +5,12 @@ import { Color } from '@/types/color';
 
 const ColorChip = ({ color, checked }: { color: Color; checked?: boolean }) => {
   return (
-    <div className="relative size-[28px] md:size-[30px]">
+    <div className="relative size-[28px] sm:size-[30px]">
       <div
         className={`size-full rounded-full ${backgroundColorConfig[color]}`}
       />
       {checked && (
-        <div className="absolute top-2/4 left-2/4 translate-x-[-50%] translate-y-[-50%] size-[22px] md:size-[24px]">
+        <div className="absolute top-2/4 left-2/4 translate-x-[-50%] translate-y-[-50%] size-[22px] sm:size-[24px]">
           <Image src={CheckIcon} alt="선택됨" fill />
         </div>
       )}
--- components/chips/tag-chip.tsx
+++ components/chips/tag-chip.tsx
@@ -5,7 +5,7 @@ import { Color } from '@/types/color';
 const TagChip = ({ children, color }: PropsWithChildren<{ color: Color }>) => {
   return (
     <span
-      className={`px-[8px] py-[4px] text-[10px] rounded-[4px] ${tagColorConfig[color].color} ${tagColorConfig[color].bgColor} md:text-xs`}
+      className={`px-[8px] py-[4px] text-[10px] rounded-[4px] ${tagColorConfig[color].color} ${tagColorConfig[color].bgColor} sm:text-xs`}
     >
       {children}
     </span>
