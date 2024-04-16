ac973a5158a891bfcb78711cb1a32bebd91e5755
whai2 <whai2125@gmail.com>
2024-04-16 15:07:36 +0900

    feat [노은수] : table 폴더 생성

--- pages/temporary-eansu.tsx
+++ pages/temporary-eansu.tsx
@@ -1,20 +1,11 @@
-//import BasicButton from "@/components/buttons/basic-button"
-import DashboardOpenButton from "@/components/buttons/domain/dashboard-open-button";
+// import InviteTable from "@/components/tables/invite-table";
 import ColorChip from "@/components/chips/color-chip";
-//import { backgroundColorConfig } from "@/constants/chips";
-import NumberChip from "@/components/chips/number-chip";
-import ProgressChip from "@/components/chips/progress-chip";
-import TagChip from "@/components/chips/tag-chip";
 
 const TemporaryEansu = () => {
   return (
     <>
-      {/* <BasicButton purpose='negative'>삭제</BasicButton> */}
-      <DashboardOpenButton >대시보드 추가</DashboardOpenButton>
-      <ColorChip color='orange' checked={false}/>
-      <NumberChip>10</NumberChip>
-      <ProgressChip>백엔드</ProgressChip>
-      <TagChip color='green'>태그</TagChip>
+      {/* <InviteTable/> */}
+      <ColorChip color='pink' checked/>
     </>
   );
 };
