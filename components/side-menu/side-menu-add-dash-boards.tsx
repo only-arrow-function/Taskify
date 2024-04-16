09844527bc11e3724f0b96a3434457840e6bf5b8
김재성 <bumpy@gimjaeseong-ui-MacBookAir.local>
2024-04-16 18:51:14 +0900

    feat [김재성]: logo와 add박스 컴포넌트 분리

---
+++ components/side-menu/side-menu-add-dash-boards.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+import addBox from '@/public/icon/add-box.svg';
+
+const SideMenuAddDashBoards = () => {
+  const addText = 'Dash Boards';
+  return (
+    <div className="w-full flex justify-between items-center mb-[22px] sm:mb-[15px]">
+      <p className="hidden sm:block text-[12px] font-bold font-[Pretendard] text-grayscale-60">
+        {addText}
+      </p>
+      <div className="relative w-[20px] h-[20px]">
+        <Image src={addBox} alt="addBox" fill />
+      </div>
+    </div>
+  );
+};
+
+export default SideMenuAddDashBoards;
