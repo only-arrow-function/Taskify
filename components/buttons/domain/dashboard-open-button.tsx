642d5758c617e6a4dc36d6d40191fec978058583
whai2 <whai2125@gmail.com>
2024-04-16 14:31:46 +0900

    refactor [노은수] : pseudo 테일윈드 사용, on-air 제거

--- components/buttons/domain/dashboard-open-button.tsx
+++ components/buttons/domain/dashboard-open-button.tsx
@@ -7,18 +7,19 @@ import DomainButtonName from './commons/domain-button-name';
 
 import ArrowForwardIcon from '@/public/icon/arrow-forward.svg';
 import CrownIcon from '@/public/icon/crown.svg';
-import OnAirIcon from '@/public/on-air.svg';
 
 interface Props {
   children?: ReactNode;
 }
 
 const DashboardOpenButton = ({ children }: Props) => {
+  const pseudoBefore = `before:content-[''] before:block before:size-[8px] before:rounded-full before:bg-green-50 mr-3`;
+
   return (
     <DashboardBase purpose="dashboard">
       <div className="flex justify-between items-center w-full px-5">
         <div className="flex items-center">
-          <Image src={OnAirIcon} alt="*" className="mr-3" />
+          <span className={`${pseudoBefore}`}/>
           <DomainButtonName purpose="dashboard">{children}</DomainButtonName>
           <Image src={CrownIcon} alt="crown" className="ml-1" />
         </div>
--- pages/temporary-eansu.tsx
+++ pages/temporary-eansu.tsx
@@ -1,23 +1,20 @@
-import BasicButton from "@/components/buttons/basic-button"
-//import LoginButton from "@/components/buttons/login-button"
-// import DashboardDeleteButton from '@/components/buttons/domain/dashboard-delete-button';
-//import DashboardOpenButton from '@/components/buttons/domain/dashboard-open-button';
-//import NewColumnAddButton from '@/components/buttons/domain/new-column-add-button';
-//import NewDashboardAddButton from '@/components/buttons/domain/new-dashboard-add-button';
-// import ColumnAddButton from "@/components/buttons/domain/column-add-button";
+//import BasicButton from "@/components/buttons/basic-button"
+import DashboardOpenButton from "@/components/buttons/domain/dashboard-open-button";
+import ColorChip from "@/components/chips/color-chip";
+//import { backgroundColorConfig } from "@/constants/chips";
+import NumberChip from "@/components/chips/number-chip";
+import ProgressChip from "@/components/chips/progress-chip";
+import TagChip from "@/components/chips/tag-chip";
 
 const TemporaryEansu = () => {
   return (
     <>
-      {/* <LoginButton isValidated={true} variant="secondary"/> */}
-      <BasicButton purpose='negative'>삭제</BasicButton>
-      {/* <NewColumnAddButton /> */}
-      {/* <ColumnAddButton /> */}
-      {/* <DashboardDeleteButton /> */}
-      {/* <NewDashboardAddButton /> */}
-      {/* <DashboardOpenButton>
-        비브리지
-      </DashboardOpenButton> */}
+      {/* <BasicButton purpose='negative'>삭제</BasicButton> */}
+      <DashboardOpenButton >대시보드 추가</DashboardOpenButton>
+      <ColorChip color='orange' checked={false}/>
+      <NumberChip>10</NumberChip>
+      <ProgressChip>백엔드</ProgressChip>
+      <TagChip color='green'>태그</TagChip>
     </>
   );
 };
