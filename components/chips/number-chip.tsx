e6f6e204ba2d4b11034ee1e030b46a052f61c77c
ayoung-iya <rhaehfdl1879@naver.com>
2024-04-17 15:12:51 +0900

    style [윤아영] : 필요없는 css 제거 및 줄바꿈 변경

--- components/chips/number-chip.tsx
+++ components/chips/number-chip.tsx
@@ -3,9 +3,7 @@ import { PropsWithChildren } from 'react';
 const NumberChip = ({ children }: PropsWithChildren) => {
   return (
     <div className="flex justify-center items-center size-[20px] bg-grayscale-30 rounded">
-      <span className="font-[Pretendard] font-medium text-xs text-grayscale-60">
-        {children}
-      </span>
+      <span className="font-medium text-xs text-grayscale-60">{children}</span>
     </div>
   );
 };
