b34100df09d0e18b1024cc8e876c4c75a0242ba6
Trophy198 <spsp8426@naver.com>
2024-04-17 16:57:31 +0900

    refactor [구승모]: 로그인 버튼 컴포넌트 style 수정

--- components/buttons/login-button.tsx
+++ components/buttons/login-button.tsx
@@ -1,27 +1,27 @@
+import { ReactNode } from 'react';
+
 interface Props {
   isValidated: boolean;
   variant: 'primary' | 'secondary';
+  type: 'submit' | 'button' | 'reset';
+  children: ReactNode;
 }
 
-const LoginButton = ({ isValidated, variant }: Props) => {
+const LoginButton = ({ isValidated }: Props) => {
   // style
-  const baseClasses = "flex items-center justify-center gap-10px flex-shrink-0 rounded-md";
-  const variantClasses = variant === 'primary'
-  ? 'w-[520px] h-[50px] py-[14px]' // primary 스타일
-  : 'w-[351px] h-[50px] py-[14px]'; // secondary 스타일
+  const baseClasses = 'flex items-center justify-center gap-10px flex-shrink-0 rounded-md';
+  const variantClasses = `w-[351px] sm:w-[520px] h-[50px] py-[14px]`;
 
   return (
     <button
       className={`${
-        isValidated ? "bg-violet-50 hover:bg-violet-50" : "bg-grayscale-50"
+        isValidated ? 'bg-violet-50 hover:bg-violet-50' : 'bg-grayscale-50'
       } focus:outline-none focus:shadow-outline cursor-${
-        isValidated ? "pointer" : "not-allowed"
+        isValidated ? 'pointer' : 'not-allowed'
       } ${baseClasses} ${variantClasses}`}
       disabled={!isValidated}
     >
-      <span className="text-white text-center font-[Pretendard] text-lg font-medium leading-normal">
-        로그인
-      </span>
+      <span className="text-white text-center font-[Pretendard] text-lg font-medium leading-normal">로그인</span>
     </button>
   );
 };
