a1396b1ac8e7ae2c554ec56b7545f684e28bf868
Trophy198 <spsp8426@naver.com>
2024-04-17 16:54:51 +0900

    feat [구승모]: 로그인 api 추가

--- apis/request.ts
+++ apis/request.ts
@@ -1,9 +1,9 @@
-import axios from "./axios"
+import axios from './axios';
 
-const ERROR_MESSAGE = "에러 발생:"
+const ERROR_MESSAGE = '에러 발생:';
 
 const requests = Object.freeze({
-  getInviteUsers: async function(dashboardid: string) {
+  getInviteUsers: async function (dashboardid: string) {
     try {
       const { data } = await axios.get(`dashboards/${dashboardid}`);
 
@@ -12,6 +12,18 @@ const requests = Object.freeze({
       console.error(ERROR_MESSAGE, error);
     }
   }, // 메서드로 다른 요청을 추가해 주세요.
-})
+  login: async function (email: string, password: string) {
+    try {
+      const { data } = await axios.post('auth/login', {
+        email,
+        password,
+      });
+      return data;
+    } catch (error) {
+      console.error(ERROR_MESSAGE, error);
+      throw error;
+    }
+  },
+});
 
-export default requests
+export default requests;
