e1246f05fd90061b7de136f129bf1912fe27b817
ayoung-iya <rhaehfdl1879@naver.com>
2024-04-17 13:50:57 +0900

    refactor [윤아영] : purpose, disabled에 따른 style을 결정하는 함수로 별도 분리

--- components/modal/modal-button.tsx
+++ components/modal/modal-button.tsx
@@ -1,19 +1,25 @@
 import { PropsWithChildren } from 'react';
 
+const determineColorStyle = (
+  purpose: 'positive' | 'negative',
+  disabled: boolean,
+) => {
+  return purpose === 'positive'
+    ? disabled
+      ? 'bg-grayscale-50 border border-grayscale-50'
+      : 'bg-violet-50 border border-violet-50'
+    : 'bg-white border border-grayscale-40';
+};
+
 const ModalButton = ({
   purpose,
-  disabled,
+  disabled = false,
   children,
 }: PropsWithChildren<{
   purpose: 'positive' | 'negative';
-  disabled?: boolean;
+  disabled: boolean;
 }>) => {
-  const colorStyle =
-    purpose === 'positive'
-      ? disabled
-        ? 'bg-grayscale-50 border border-grayscale-50'
-        : 'bg-violet-50 border border-violet-50'
-      : 'bg-white border border-grayscale-40';
+  const colorStyle = determineColorStyle(purpose, disabled);
   const fontStyle = `text-sm font-medium ${
     purpose === 'positive' ? 'text-white' : 'text-grayscale-60'
   } sm:text-base`;
