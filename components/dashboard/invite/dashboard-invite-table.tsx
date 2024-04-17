92ec06814ec0c12d3f9970a73edfc572c60fcffd
d5ng <dong5ffice@gmail.com>
2024-04-17 11:51:24 +0900

    Feat: dashboard-invite PC

--- components/dashboard/invite/dashboard-invite-table.tsx
+++ components/dashboard/invite/dashboard-invite-table.tsx
@@ -1,7 +1,40 @@
+import BasicButton from '@/components/buttons/basic-button';
+
 const DashboardInviteTable = () => {
   return (
     <div>
-      <div></div>
+      <div className="flex text-grayscale-50">
+        <span className="w-[36.025%]">이름</span>
+        <span className="w-[31.263%]">초대자</span>
+        <span className="w-[32.712%]">수락 여부</span>
+      </div>
+
+      <ul>
+        <li className="flex items-center py-7 text-grayscale-80 border-b border-grayscale-30">
+          <span className="w-[36.025%]">프로덕트 디자인</span>
+          <span className="w-[31.263%]">손동희</span>
+          <span className="w-[32.712%] flex gap-2.5">
+            <BasicButton purpose="positive">수락</BasicButton>
+            <BasicButton purpose="negative">거절</BasicButton>
+          </span>
+        </li>
+        <li className="flex items-center py-7 text-grayscale-80 border-b border-grayscale-30">
+          <span className="w-[36.025%]">프로덕트 디자인</span>
+          <span className="w-[31.263%]">손동희</span>
+          <span className="w-[32.712%] flex gap-2.5">
+            <BasicButton purpose="positive">수락</BasicButton>
+            <BasicButton purpose="negative">거절</BasicButton>
+          </span>
+        </li>
+        <li className="flex items-center py-7 text-grayscale-80 border-b border-grayscale-30">
+          <span className="w-[36.025%]">프로덕트 디자인</span>
+          <span className="w-[31.263%]">손동희</span>
+          <span className="w-[32.712%] flex gap-2.5">
+            <BasicButton purpose="positive">수락</BasicButton>
+            <BasicButton purpose="negative">거절</BasicButton>
+          </span>
+        </li>
+      </ul>
     </div>
   );
 };
--- components/dashboard/invite/dashboard-invite.tsx
+++ components/dashboard/invite/dashboard-invite.tsx
@@ -4,7 +4,7 @@ import DashboardInviteTable from '@/components/dashboard/invite/dashboard-invite
 
 const DashboardInvite = () => {
   return (
-    <section className="w-[1022px] h-[400px] bg-white rounded-lg">
+    <section className="w-[1022px] h-[600px] bg-white rounded-lg md:w-[67.742vw] lg:w-[1022px]">
       <div className="px-7 pt-8">
         <h3 className="text-2xl font-bold">초대받은 대시보드</h3>
         <DashboardInviteSearch />
