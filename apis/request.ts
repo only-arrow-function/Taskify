e4696689820863e74fdba05361d719512a8e10c3
whai2 <whai2125@gmail.com>
2024-04-18 12:36:02 +0900

    fix [노은수] : 삭제된 파일 복구

---
+++ apis/request.ts
@@ -0,0 +1,32 @@
+import axios from './axios';
+
+const ERROR_MESSAGE = '에러 발생:';
+
+const requests = Object.freeze({
+  getInviteUsers: async (dashboardid: string, token: string|null) => {
+    if (!token) return;
+    
+    try {
+      const { data } = await axios.get(`dashboards/${dashboardid}`, {
+        headers: { Authorization: 'Bearer ' + token }
+      });
+      return data;
+    } catch (error) {
+      console.error(ERROR_MESSAGE, error);
+    }
+  },
+
+  login: async (email: string, password: string) => {
+    try {
+      const { data } = await axios.post('auth/login', { email, password });
+      return data;
+    } catch (error) {
+      console.error(ERROR_MESSAGE, error);
+      throw error;
+    }
+  },
+
+  // 다른 API 요청도 여기에 추가 가능
+});
+
+export default requests;
