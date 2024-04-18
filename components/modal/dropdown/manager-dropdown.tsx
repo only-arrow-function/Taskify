7db47c4e47c31b43b467d4e687a97a2404232f6f
whai2 <whai2125@gmail.com>
2024-04-18 16:43:46 +0900

    feat [노은수] : 담당자 드롭다운 구현, 로직은 담지 못함

--- components/modal/dropdown/manager-dropdown.tsx
+++ components/modal/dropdown/manager-dropdown.tsx
@@ -7,18 +7,25 @@ const ManagerDropdown = ({ placeholder }: { placeholder: string }) => {
   const [isOpenDropdown, setIsOpenDropdown] = useState(false);
 
   const handleOpenDropdown = () => {
-    setIsOpenDropdown((isOpenDropdown) => !isOpenDropdown);
+    setIsOpenDropdown(true);
+  }
+
+  const handleCloseDropdown = () => {
+    setIsOpenDropdown(false);
   }
 
   return (
     <div className="flex flex-col gap-[10px]">
-      <label className="text-grayscale-80 mb-[0.5rem] text-base font-medium">
+      <label className="text-grayscale-80 text-base font-medium">
         담당자
       </label>
-      <div className="flex w-full h-[3.125rem] px-4 rounded-lg border">
-        <input placeholder={placeholder} className="w-full"/>
-        <Image src={ArrowDownIcon} alt="아래 화살표" className="relative right-[16px]"/>
+      <div className="flex w-full h-[3.125rem] px-4 rounded-lg border focus-within:border-violet-50" onClick={handleOpenDropdown}>
+        <input placeholder={placeholder} className="w-full outline-none"/>
+        <Image src={ArrowDownIcon} alt="아래 화살표" className="relative"/>
       </div>
+      {isOpenDropdown && 
+        <div className="flex w-full h-[3.125rem] px-4 rounded-lg border absolute top-[90px]"></div>
+      }
     </div>
   )
 }
