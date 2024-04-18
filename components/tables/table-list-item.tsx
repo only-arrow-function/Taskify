1c47df7287c965a106eac91627a6e82eb41f6084
ayoung-iya <rhaehfdl1879@naver.com>
2024-04-18 12:10:13 +0900

    feat [윤아영] : 멤버 삭제 기능 구현
    useSWR의 mutate로 상태 업데이트
    delete api가 에러가 났을 때, mutate가 일어나지 않도록 async,,,await 사용

--- components/tables/table-list-item.tsx
+++ components/tables/table-list-item.tsx
@@ -1,18 +1,37 @@
+import { useSWRConfig } from 'swr';
 import { Member } from './members-type';
-import BasicButton from '../buttons/basic-button';
+import TableButton from './table-button';
+import requests from '@/apis/request';
+// import { useGetMembers } from '@/hooks/swr/use-members';
 
-const TableListItem = ({ member }: { member: Member }) => {
+const TableListItem = ({ member, members }: { member: Member; members: Member[] }) => {
+  const { mutate } = useSWRConfig();
+
+  const handleDeleteMember = (deleteId: number) => async () => {
+    const updateMembers = (deleteId: number) => {
+      const newMembers = members.filter((member) => member.id !== deleteId);
+      return { members: newMembers, totalCount: newMembers.length };
+    };
+  
+    try {
+      await requests.deleteMembers(deleteId);
+      mutate('members', updateMembers(deleteId));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <li className="flex justify-between py-3 shadow-b-inner-30 px-5 first-of-type:pt-5 last-of-type:shadow-none last-of-type:pb-0 sm:px-7 sm:py-4 sm:first-of-type:pt-6">
+    <li className="flex justify-between items-center py-3 shadow-b-inner-30 px-5 first-of-type:pt-5 last-of-type:shadow-none last-of-type:pb-0 sm:px-7 sm:py-4 sm:first-of-type:pt-6">
       <div className="flex items-center gap-2 sm:gap-3">
         <span className="inline-flex justify-center items-center size-[34px] rounded-full bg-[#C3B1A2] font-[Montserrat] text-sm font-semibold text-white sm:size-[38px] sm:text-base">
           {member.email[0].toUpperCase()}
         </span>
-        <span className="text-grayscale-80 text-sm sm:text-base">
-          {member.nickname}
-        </span>
+        <span className="text-grayscale-80 text-sm sm:text-base">{member.nickname}</span>
       </div>
-      <BasicButton purpose="negative">삭제</BasicButton>
+      <TableButton purpose="negative" onClick={handleDeleteMember(member.id)}>
+        삭제
+      </TableButton>
     </li>
   );
 };
