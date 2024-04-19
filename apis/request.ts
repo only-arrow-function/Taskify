4c53bb745053a50f0a938a8cd098c289f0e483f4
ayoung-iya <rhaehfdl1879@naver.com>
2024-04-19 23:24:35 +0900

    refactor [윤아영] : getMembers page 추가, deleteMembers header 추가

--- apis/request.ts
+++ apis/request.ts
@@ -55,11 +55,11 @@ const requests = Object.freeze({
 
   // 다른 API 요청도 여기에 추가 가능
 
-  getMembers: async (dashboardId: string): Promise<Members> => {
+  getMembers: async (dashboardId: string, currentPage: number): Promise<Members> => {
     try {
       if (!token) throw new Error('토큰이 없어요. 다시 로그인 해주세요.');
 
-      const { data } = await axios.get(`members?page=1&size=4&dashboardId=${dashboardId}`, headers);
+      const { data } = await axios.get(`members?page=${currentPage}&size=4&dashboardId=${dashboardId}`, headers);
       return data;
     } catch (error) {
       console.log(ERROR_MESSAGE, error);
@@ -72,7 +72,7 @@ const requests = Object.freeze({
 
   deleteMembers: async (memberId: number) => {
     try {
-      await axios.delete(`/members/${memberId}`);
+      await axios.delete(`members/${memberId}`, headers);
     } catch (error) {
       const err = error as Error;
       throw new Error(err.message);
