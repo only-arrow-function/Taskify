bc21c8b1770dc6234009ef681ff14f7feef7bc7b
whai2 <whai2125@gmail.com>
2024-04-19 17:12:38 +0900

    feat [노은수] : 드롭다운 훅, 2가지로 분리, 하나는 상태 관리, 다른 하나는 outside 클릭에 대한 처리

--- components/modal/dropdown/manager-dropdown.tsx
+++ components/modal/dropdown/manager-dropdown.tsx
@@ -1,15 +1,20 @@
 import Image from "next/image";
 
+import { useHandleDropdown, useHandleDropdownOutside } from "@/hooks/use-handle-dropdown";
+
 import ArrowDownIcon from "@/public/icon/arrow-drop-down.svg"
 
-const ManagerDropdown = ({ placeholder, openDropdown, isOpenDropdown }: { placeholder: string, openDropdown: () => void, isOpenDropdown: boolean}) => {
+const ManagerDropdown = ({ placeholder }: { placeholder: string }) => {
+  const { isOpenDropdown, handleOpenDropdown, handleCloseDropdown } = useHandleDropdown();
+  const initRef = useHandleDropdownOutside(handleOpenDropdown, handleCloseDropdown);
+  
   return (
-    <>
+    <div ref={initRef}>
       <div className="flex flex-col">
         <label className="text-grayscale-80 text-base font-medium">
           담당자
         </label>
-        <div className="flex sm:w-[217px] w-[287px] h-[3.125rem] px-4 rounded-lg border focus-within:border-violet-50" onClick={openDropdown}>
+        <div className="flex sm:w-[217px] w-[287px] h-[3.125rem] px-4 rounded-lg border focus-within:border-violet-50">
           <input placeholder={placeholder} className="w-full outline-none"/>
           <Image src={ArrowDownIcon} alt="아래 화살표" />
         </div>
@@ -18,7 +23,7 @@ const ManagerDropdown = ({ placeholder, openDropdown, isOpenDropdown }: { placeh
         <div className="flex sm:w-[217px] w-[287px] h-[3.125rem] rounded-lg border mt-1 absolute bg-white sm:top-[167px] sm:right-[36px]"></div>
       }
       <div className="flex w-full h-[1.5rem]"/>
-    </>
+    </div>
   )
 }
 
--- pages/temporary-eansu.tsx
+++ pages/temporary-eansu.tsx
@@ -1,12 +1,11 @@
-import ManagerDropdown from '@/components/modal/dropdown/manager-dropdown';
-import StateDropdown from '@/components/modal/dropdown/state-dropdown';
-import Modal from '@/components/modal/modal';
-import ModalNewTodo from '@/components/modal/modal-new-todo';
+import ModalEditTodo from '@/components/modal/modal-edit-todo';
+// import ModalNewTodo from "@/components/modal/modal-new-todo";
 
 const TemporaryEansu = () => {
   return (
-    // <ManagerDropdown placeholder='이름을 입력해 주세요'/>
-    <StateDropdown/>
+    <>
+      <ModalEditTodo />
+    </>
   )
 };
 
