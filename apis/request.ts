cb4d7413a6f33e67c0c634b25f8e4dfdb486cfc2
ayoung-iya <rhaehfdl1879@naver.com>
2024-04-19 15:32:51 +0900

    feat [윤아영] : 카드이미지, 카드 post api 구현

--- apis/request.ts
+++ apis/request.ts
@@ -1,14 +1,15 @@
 import axios from './axios';
+// import { StatesData } from '@/components/modal/modal-new-todo';
 
 const ERROR_MESSAGE = '에러 발생:';
 
 const requests = Object.freeze({
-  getInviteUsers: async (dashboardid: string, token: string|null) => {
+  getInviteUsers: async (dashboardid: string, token: string | null) => {
     if (!token) return;
-    
+
     try {
       const { data } = await axios.get(`dashboards/${dashboardid}`, {
-        headers: { Authorization: 'Bearer ' + token }
+        headers: { Authorization: 'Bearer ' + token },
       });
       return data;
     } catch (error) {
@@ -26,7 +27,35 @@ const requests = Object.freeze({
     }
   },
 
-   // 다른 API 요청도 여기에 추가 가능
+  // 다른 API 요청도 여기에 추가 가능
+
+  postCardImage: async (columnId: number, imageData: FormData) => {
+    const accessToken = window.localStorage.getItem('accessToken');
+
+    try {
+      const { data } = await axios.post(`columns/${columnId}/card-image`, imageData, {
+        headers: { Authorization: 'Bearer ' + accessToken, 'Content-Type': 'multipart/form-data' },
+      });
+
+      return data.imageUrl;
+    } catch (error) {
+      console.log(ERROR_MESSAGE, error);
+    }
+  },
+
+  postCard: async (cardInfo: any) => {
+    const accessToken = window.localStorage.getItem('accessToken');
+
+    try {
+      const { data } = await axios.post('cards', cardInfo, {
+        headers: { Authorization: 'Bearer ' + accessToken },
+      });
+      
+      return data;
+    } catch (error) {
+      console.log(ERROR_MESSAGE, error);
+    }
+  },
 });
 
 export default requests;
