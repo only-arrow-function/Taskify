a81af1d222f5794fab88e89eff6e27bcd30291b0
ayoung-iya <rhaehfdl1879@naver.com>
2024-04-19 23:25:42 +0900

    feat [윤아영] : 페이지네이션 기능 추가

--- components/tables/members-table.tsx
+++ components/tables/members-table.tsx
@@ -1,19 +1,28 @@
 // import { useParams } from 'next/navigation';
 import { useState } from 'react';
 import TableListItem from './table-list-item';
+import DashboardPaginationButton from '../buttons/pagination/dashboard-pagination-button';
 import { useGetMembers } from '@/hooks/swr/use-members';
 
-const temporaryId = '5994';
-
-const MembersTable = () => {
-  // const { id } = useParams();
+const MembersTable = ({ dashboardId }: { dashboardId: string }) => {
   const [currentPage, setCurrentPage] = useState(1);
-  const { data } = useGetMembers(temporaryId);
+
+  const { data } = useGetMembers(dashboardId, currentPage);
 
   if (!data) return null;
 
   const { members, totalCount } = data;
-  const totalPage = totalCount > 4 ? Math.floor(totalCount / 4) : 1;
+  const totalPage = Math.ceil(totalCount / 4);
+
+  const handleLeftButtonClick = () => {
+    if (currentPage === 1) return;
+    setCurrentPage(currentPage - 1);
+  };
+
+  const handleRightButtonClick = () => {
+    if (currentPage === totalPage) return;
+    setCurrentPage(currentPage + 1);
+  };
 
   return (
     <div className="flex flex-col justify-between min-w-[284px] max-w-[620px] rounded-lg bg-white pt-[22px] pb-4 sm:pt-[26px] sm:pb-5">
@@ -23,12 +32,20 @@ const MembersTable = () => {
           <span className="text-xs text-grayscale-80 sm:text-sm">
             {currentPage} 페이지 중 {totalPage}
           </span>
+          <div className="flex">
+            <DashboardPaginationButton onClick={handleLeftButtonClick} position="left" isDisabled={currentPage === 1} />
+            <DashboardPaginationButton
+              onClick={handleRightButtonClick}
+              position="right"
+              isDisabled={currentPage === totalPage}
+            />
+          </div>
         </div>
       </header>
       <h4 className="px-5 text-sm leading-[17px] text-grayscale-50 sm:text-base sm:px-7 sm:leading-[19px]">이름</h4>
       <ul>
         {members?.map((member) => (
-          <TableListItem key={member.id} member={member} members={members} />
+          <TableListItem key={member.id} member={member} members={members} currentPage={currentPage} dashboardId={dashboardId} />
         ))}
       </ul>
     </div>
