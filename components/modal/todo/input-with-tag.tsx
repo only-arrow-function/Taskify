c07e48c0f771346ceb3342ec41416fb7222a627b
ayoung-iya <rhaehfdl1879@naver.com>
2024-04-20 16:31:22 +0900

    feat [윤아영] : 랜덤 색상 적용

--- components/modal/todo/input-with-tag.tsx
+++ components/modal/todo/input-with-tag.tsx
@@ -1,5 +1,7 @@
 import { KeyboardEventHandler } from 'react';
+import type { Color } from '@/components/chips/color.type';
 import TagChip from '@/components/chips/tag-chip';
+import { chooseRandomColor } from '@/util/randomColor';
 
 const InputWithTag = ({
   label,
@@ -19,13 +21,11 @@ const InputWithTag = ({
   onRemoveTag: () => void;
 }) => {
   const handleKeyDown: KeyboardEventHandler = (e) => {
-    e.stopPropagation();
-    e.preventDefault();
     const target = e.target as HTMLInputElement;
-
     if (e.key !== 'Enter' && e.key !== 'Backspace') return;
 
     if (e.key === 'Enter') {
+      e.preventDefault();
       if (target.value.trim() === '') {
         return;
       }
@@ -34,7 +34,8 @@ const InputWithTag = ({
         return;
       }
 
-      const addTag = target.value;
+      const randomColor = chooseRandomColor();
+      const addTag = target.value + '$' + randomColor;
       onAddTag(addTag);
       target.value = '';
 
@@ -43,10 +44,11 @@ const InputWithTag = ({
 
     if (e.key === 'Backspace') {
       if (target.value === '' && tags.length !== 0) {
+        e.preventDefault();
+        target.value = tags[tags.length - 1].split('$')[0];
         onRemoveTag();
         return;
       }
-      return;
     }
   };
 
@@ -56,11 +58,16 @@ const InputWithTag = ({
         {label}
       </label>
       <div className="flex flex-wrap items-center gap-[6px] w-full pt-[13px] pb-3 px-4 rounded-lg border sm:pt-[15px] sm:pb-[14px]">
-        {tags?.map((tag) => (
-          <TagChip color="green" key={tag}>
-            {tag}
-          </TagChip>
-        ))}
+        {tags?.map((tag) => {
+          const [name, colorString] = tag.split('$');
+          const color = colorString as Color;
+
+          return (
+            <TagChip color={color} key={tag}>
+              {name}
+            </TagChip>
+          );
+        })}
         <input
           id={id}
           type={type}
