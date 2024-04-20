bbda30f40ce7de04d337934fba39483741a93a1d
ayoung-iya <rhaehfdl1879@naver.com>
2024-04-20 17:50:44 +0900

    refactor [윤아영] : isFormFilled 수정

--- lib/domain/is-form-filled.ts
+++ lib/domain/is-form-filled.ts
@@ -1,12 +1,7 @@
-import { StatesData } from "@/components/modal/modal-new-todo";
+import { StatesData } from '@/components/modal/modal-new-todo';
 
 export const isFormFilled = (data: any) => {
   const statesKeys = Object.keys(data) as (keyof StatesData)[];
-  //console.log(data)
 
-  for (const key of statesKeys) {
-    if (!data[key]) return false;
-    if (key === 'tags' && data[key].length === 0) return false;
-  }
-  return true;
-}
+  return statesKeys.every((key) => data[key].length);
+};
