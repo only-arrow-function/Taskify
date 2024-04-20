a50f66d8b85e3334ce2b677fde9ba56e425ae102
ayoung-iya <rhaehfdl1879@naver.com>
2024-04-21 03:32:04 +0900

    style [윤아영] : z-index 추가, 줄변경

--- components/modal/modal-newTodo-layout.tsx
+++ components/modal/modal-newTodo-layout.tsx
@@ -1,13 +1,10 @@
 import { PropsWithChildren } from 'react';
 
 const ModalNewTodoLayout = ({ children }: PropsWithChildren) => {
-  const sizeStyles =
-    'w-[327px] px-5 py-7 pb-5 sm:w-[506px] sm:px-7 sm:pt-8 sm:pb-7';
+  const sizeStyles = 'w-[327px] px-5 py-7 pb-5 sm:w-[506px] sm:px-7 sm:pt-8 sm:pb-7';
 
   return (
-    <div
-      className={`fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white rounded-lg ${sizeStyles}`}
-    >
+    <div className={`fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white rounded-lg ${sizeStyles} z-30`}>
       {children}
     </div>
   );
