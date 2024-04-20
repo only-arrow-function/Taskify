853a687a9cf6bd1771eb5839687e855435e5a973
ayoung-iya <rhaehfdl1879@naver.com>
2024-04-21 03:33:54 +0900

    chore [윤아영] : 이미지 관련 config 추가

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['sprint-fe-project.s3.ap-northeast-2.amazonaws.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'sprint-fe-project.s3.ap-northeast-2.amazonaws.com',
+        port: '',
+        pathname: '/taskify/task_image/**',
+      },
+    ],
   },
 };
 
