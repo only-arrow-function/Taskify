ac1c929f62a8b92b751503a23be81de59015e369
whai2 <whai2125@gmail.com>
2024-04-21 19:28:06 +0900

    feat [노은수] : 초대 테이블의 초대 모달 구현

--- components/dashboard/modal/Invite-modal.tsx
+++ components/dashboard/modal/Invite-modal.tsx
@@ -1,12 +1,12 @@
-import { ChangeEvent, useState } from "react";
-import { useQueryClient } from "@tanstack/react-query";
+import { ChangeEvent, useState } from 'react';
+import { useQueryClient } from '@tanstack/react-query';
 
 import BasicButton from '@/components/buttons/basic-button';
 import InputField from '@/components/inputs/input-field';
 import ModalTitle from '@/components/modal/modal-title';
 import ModalWithDimmed from '@/components/modal/modal-with-dimmed';
 
-import { useInvitationsMutation } from "@/hooks/react-query/use-query-invite-users";
+import { useInvitationsMutation } from '@/hooks/react-query/use-query-invite-users';
 
 interface InviteModalType {
   handleCloseModal: () => void;
@@ -19,22 +19,25 @@ const InviteModal = ({ handleCloseModal, dashboardId, totalPages }: InviteModalT
 
   // server state
   const queryClient = useQueryClient();
-  const { mutateAsync } = useInvitationsMutation(dashboardId, { email: input }, queryClient)
+  const { mutateAsync } = useInvitationsMutation(dashboardId, { email: input }, queryClient);
 
-  const handleInputChange = (e:ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
     setInput(e.target.value);
-  }
+  };
 
   const handleClickForInvite = async () => {
     try {
-      await mutateAsync({dashboardId, input}, {
-        onSuccess: () => {},
-      });
+      await mutateAsync(
+        { dashboardId, input },
+        {
+          onSuccess: () => {},
+        },
+      );
 
       handleCloseModal();
     } catch (error) {
-      console.error("에러 발생:", error);
+      console.error('에러 발생:', error);
     }
   };
 
@@ -49,9 +52,13 @@ const InviteModal = ({ handleCloseModal, dashboardId, totalPages }: InviteModalT
         onChange={handleInputChange}
         value={input}
       />
-      <div className='flex flex-row gap-[10px] justify-end'>
-        <BasicButton purpose='negative' eventHandler={handleCloseModal}>취소</BasicButton>
-        <BasicButton purpose='positive' eventHandler={handleClickForInvite}>초대</BasicButton>
+      <div className="flex flex-row gap-[10px] justify-end">
+        <BasicButton purpose="negative" eventHandler={handleCloseModal}>
+          취소
+        </BasicButton>
+        <BasicButton purpose="positive" eventHandler={handleClickForInvite}>
+          초대
+        </BasicButton>
       </div>
     </ModalWithDimmed>
   );
