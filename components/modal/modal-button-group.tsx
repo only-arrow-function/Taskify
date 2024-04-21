e6d543bf6e3677ddc6a3324a9bc15c7624007021
ayoung-iya <rhaehfdl1879@naver.com>
2024-04-22 02:33:57 +0900

    refactor [윤아영] : button onClick 핸들러 추가, disabled 스타일 가상선택자로 적용

--- components/modal/modal-button-group.tsx
+++ components/modal/modal-button-group.tsx
@@ -5,16 +5,18 @@ type PositiveName = '생성' | '확인' | '수정' | '변경' | '삭제';
 const ModalButtonGroup = ({
   positiveName,
   disabled,
+  onClick,
 }: {
   positiveName: PositiveName;
-  disabled?: boolean;
+  disabled: boolean;
+  onClick: () => void;
 }) => {
   return (
     <div className="flex justify-center gap-3 sm:justify-end">
       <ModalButton purpose="negative" disabled={false}>
         취소
       </ModalButton>
-      <ModalButton purpose="positive" disabled={!disabled}>
+      <ModalButton purpose="positive" disabled={disabled} onClick={onClick}>
         {positiveName}
       </ModalButton>
     </div>
