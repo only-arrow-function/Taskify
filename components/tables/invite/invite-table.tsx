edf00e13ce2371b9e084660bce8afb2d53305c1b
whai2 <whai2125@gmail.com>
2024-04-22 19:59:36 +0900

    feat [노은수] : 삭제하기 기능 추가, 뮤테이션 적용

--- components/tables/invite/invite-table.tsx
+++ components/tables/invite/invite-table.tsx
@@ -39,19 +39,19 @@ const InviteTable = ({ dashboardId }: DashboardIdProps) => {
     <div className="flex w-full px-[28px] py-[32px] flex-col rounded-md bg-white">
       <div className="flex items-center justify-between mb-[20px]">
         <span className="text-center text-lg font-bold">초대 내역</span>
-          {data && !!data.totalPages && (
-            <div className="flex items-center gap-[10px]">
-              <span className="text-xs text-grayscale-80 sm:text-sm">{currentPage}페이지</span>
-                <div className="flex">
-                  <DashboardPaginationButton onClick={prevPage} isDisabled={currentPage === 1} position="left" />
-                  <DashboardPaginationButton
-                    onClick={nextPage}
-                    isDisabled={currentPage >= data.totalPages}
-                    position="right"
-                  />
-                </div>
+        {data && !!data.totalPages && (
+          <div className="flex items-center gap-[10px]">
+            <span className="text-xs text-grayscale-80 sm:text-sm">{currentPage}페이지</span>
+            <div className="flex">
+              <DashboardPaginationButton onClick={prevPage} isDisabled={currentPage === 1} position="left" />
+              <DashboardPaginationButton
+                onClick={nextPage}
+                isDisabled={currentPage >= data.totalPages}
+                position="right"
+              />
             </div>
-          )}
+          </div>
+        )}
       </div>
       <div className="flex justify-between items-center mb-[10px]">
         <span className="text-grayscale-50 text-sm font-normal leading-normal">이메일</span>
@@ -60,27 +60,27 @@ const InviteTable = ({ dashboardId }: DashboardIdProps) => {
         </BasicButton>
       </div>
       <ul className="flex flex-col items-center justify-between">
-        {isPending ? (<InviteTableSkeleton />) : (
-          data.pages[currentPage - 1].invitations.length ? (
-            data.pages[currentPage - 1].invitations.map(
-              ({ id, invitee, inviteAccepted }: InvitationsDataProps<InviteeType>) => (
-                <li
-                  key={id}
-                  className="w-full flex flex-row justify-between items-center border-b border-grayscale-30 py-[12px]"
-                >
-                  <span>{invitee.email}</span>
-                  <InviteCancelButton purpose="negative" invitationId={id} dashboardId={dashboardId}>
-                    취소
-                  </InviteCancelButton>
-                </li>
-              ),
-            )
-          ) : (
-            <>
-              <Image src={NoEmailIcon} alt="빈 이메일" />
-              <span className="text-grayscale-40">아직 초대한 멤버가 없어요</span>
-            </>
+        {isPending ? (
+          <InviteTableSkeleton />
+        ) : data.pages[currentPage - 1].invitations.length ? (
+          data.pages[currentPage - 1].invitations.map(
+            ({ id, invitee, inviteAccepted }: InvitationsDataProps<InviteeType>) => (
+              <li
+                key={id}
+                className="w-full flex flex-row justify-between items-center border-b border-grayscale-30 py-[12px]"
+              >
+                <span>{invitee.email}</span>
+                <InviteCancelButton purpose="negative" invitationId={id} dashboardId={dashboardId}>
+                  취소
+                </InviteCancelButton>
+              </li>
+            ),
           )
+        ) : (
+          <>
+            <Image src={NoEmailIcon} alt="빈 이메일" />
+            <span className="text-grayscale-40">아직 초대한 멤버가 없어요</span>
+          </>
         )}
       </ul>
       {isOpenModal && (
