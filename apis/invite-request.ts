9a84925c7ebddf1d8bb1e317df519892b2bce95d
whai2 <whai2125@gmail.com>
2024-04-22 19:59:36 +0900

    feat [노은수] : 삭제하기 기능 추가, 뮤테이션 적용

--- apis/invite-request.ts
+++ apis/invite-request.ts
@@ -1,4 +1,7 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> edf00e1 (feat [노은수] : 삭제하기 기능 추가, 뮤테이션 적용)
 import axios from './axios';
 import getToken from './localStorage';
 
@@ -10,18 +13,26 @@ const headers = {
 };
 
 const inviteRequests = Object.freeze({
+<<<<<<< HEAD
   getInviteUsers: async (dashboardId: string, page: unknown | number) => {
 <<<<<<< HEAD
     console.log('들어갔습니다.');
     await new Promise((resolve) => setTimeout(() => resolve(1), 1000));
 =======
 >>>>>>> e84a234 (feat [노은수] : 사이드바 리액트 쿼리 데이터 연동)
+=======
+  getInviteUsers: async (dashboardId: string, page: number) => {
+    console.log('들어갔습니다.');
+>>>>>>> edf00e1 (feat [노은수] : 삭제하기 기능 추가, 뮤테이션 적용)
     try {
       const { data } = await axios.get(`dashboards/${dashboardId}/invitations`, {
         headers: { Authorization: `Bearer ${token}` },
         params: {
           page,
+<<<<<<< HEAD
           size: 5,
+=======
+>>>>>>> edf00e1 (feat [노은수] : 삭제하기 기능 추가, 뮤테이션 적용)
         },
       });
       return data;
@@ -38,6 +49,7 @@ const inviteRequests = Object.freeze({
       console.error(ERROR_MESSAGE, error);
     }
   },
+<<<<<<< HEAD
 
   deleteInvitedUser: async (dashboardId: string, invitationId: string) => {
     try {
@@ -52,3 +64,8 @@ const inviteRequests = Object.freeze({
 export default inviteRequests;
 =======
 >>>>>>> 6be2eae (feat [노은수] : swr을 이용한 페이지네이션 구현)
+=======
+});
+
+export default inviteRequests;
+>>>>>>> edf00e1 (feat [노은수] : 삭제하기 기능 추가, 뮤테이션 적용)
