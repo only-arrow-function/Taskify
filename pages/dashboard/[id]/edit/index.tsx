cd39b05658f4c4a20e12f9eca0f4e48044e20f82
whai2 <whai2125@gmail.com>
2024-04-22 19:59:36 +0900

    feat [노은수] : 삭제하기 기능 추가, 뮤테이션 적용

--- pages/dashboard/[id]/edit/index.tsx
+++ pages/dashboard/[id]/edit/index.tsx
@@ -2,7 +2,7 @@ import { GetServerSideProps } from 'next';
 
 import EditDashboard from '@/components/dashboard/edit/edit-dashboard';
 import ReturnDashboardPage from '@/components/dashboard/edit/return-dashboard-page';
-import InviteTable from '@/components/tables/invite-table';
+import InviteTable from '@/components/tables/invite/invite-table';
 import MembersTable from '@/components/tables/members-table';
 import { IdProps } from '@/constant/type/data/dashboard.type';
 
