9ee0edbf06039bc6b7911440bc5dd5e7c110bf7c
d5ng <dong5ffice@gmail.com>
2024-04-23 15:07:59 +0900

    Feat: Zustand 로컬 스토리지 저장

--- pages/my-dashboard/index.tsx
+++ pages/my-dashboard/index.tsx
@@ -1,19 +1,10 @@
-import { GetStaticProps } from 'next';
-import requests from '@/apis/request';
 import DashboardHeader from '@/components/dashboard/header/dashboard-header';
 import DashboardInvite from '@/components/dashboard/invite/dashboard-invite';
 import DashboardSectionLayout from '@/components/dashboard/layout/dashboard-section-layout';
 import DashboardList from '@/components/dashboard/list/dashboard-list';
 import SideMenu from '@/components/side-menu/side-menu';
-import { DashboardResponse } from '@/hooks/swr/dashboard/use-dashboards';
-import { useAuthenticationStore } from '@/store/auth';
 
 const MyDashboard = () => {
-  const nickname = useAuthenticationStore((state) => state.nickname);
-  const profileImage = useAuthenticationStore((state) => state.profileImageUrl);
-
-  console.log(nickname, profileImage);
-
   return (
     <DashboardSectionLayout>
       <DashboardHeader />
