33c45d5bcb298f37277a8324bb4585f7ac3d9d17
ayoung-iya <rhaehfdl1879@naver.com>
2024-04-24 01:42:13 +0900

    chore [윤아영] : 프로필 이미지 관련 config 추가

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,12 @@ const nextConfig = {
         port: '',
         pathname: '/taskify/task_image/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'sprint-fe-project.s3.ap-northeast-2.amazonaws.com',
+        port: '',
+        pathname: '/taskify/profile_image/**',
+      },
     ],
   },
 };
