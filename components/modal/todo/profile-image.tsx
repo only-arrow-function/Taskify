1ec2949dba714c575afa73194644c177f71fdb2a
ayoung-iya <rhaehfdl1879@naver.com>
2024-04-24 03:11:07 +0900

    feat [윤아영] : (임시) 프로필 이미지 컴포넌트

---
+++ components/modal/todo/profile-image.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image';
+
+const ProfileImage = ({ nickname, imageUrl }) => {
+  return (
+    <div className="relative size-[1.625rem] rounded-full overflow-hidden">
+      {imageUrl && <Image src={imageUrl} alt="프로필 이미지" fill style={{ objectFit: 'cover' }} />}
+      {nickname && (
+        <span className="inline-flex justify-center items-center rounded-full text-xs font-[Montserrat] bg-[#A3C4A2] size-full text-white border-2 border-white">
+          {nickname.slice(0, 1)}
+        </span>
+      )}
+      
+    </div>
+  );
+};
+
+export default ProfileImage;
