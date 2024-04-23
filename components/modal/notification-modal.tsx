4d0f91c8034dd4757aaa5c96ac0acb6e4598ee8c
Trophy198 <spsp8426@naver.com>
2024-04-24 00:27:09 +0900

    refactor [구승모]: notification 모달에 keyDown 엔터키 추가 및 스크롤 감지 추가 - 모달이 ref탐지로 포커싱 되면서 스크롤이 아래로 내려가는 현상이 있어 스크롤을 감지하여 사용자가 자연스러운 사용을 하기위함

--- components/modal/notification-modal.tsx
+++ components/modal/notification-modal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Modal from './modal';
 import ModalButton from './modal-button';
 import ModalDescription from './modal-description';
@@ -6,18 +6,43 @@ import { useToggleStore } from '@/store/toggle-store';
 
 const NotificationModal = ({ message }: { message: string }) => {
   const { handleCloseToggle } = useToggleStore();
+  const modalRef = useRef<HTMLDivElement>(null);
+  const scrollPosition = useRef<number>(0);
+
+  useEffect(() => {
+    scrollPosition.current = window.scrollY;
+    modalRef.current?.focus();
+
+    return () => {
+      window.scrollTo({ top: scrollPosition.current });
+    };
+  }, []);
+
+  const handleKeyDown = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      handleCloseToggle();
+    }
+  };
 
   return (
-    <Modal>
-      <div className="flex flex-col h-full justify-between">
-        <ModalDescription text={message} />
-        <div className="flex justify-center mt-[10px] sm:justify-end">
-          <ModalButton disabled={false} purpose="positive" onClick={handleCloseToggle}>
-            확인
-          </ModalButton>
+    <div
+      ref={modalRef}
+      tabIndex={-1}
+      onKeyDown={handleKeyDown}
+      className="outline-none"
+      onFocus={() => window.scrollTo({ top: scrollPosition.current })}
+    >
+      <Modal>
+        <div className="flex flex-col h-full justify-between">
+          <ModalDescription text={message} />
+          <div className="flex justify-center mt-[10px] sm:justify-end">
+            <ModalButton disabled={false} purpose="positive" onClick={handleCloseToggle}>
+              확인
+            </ModalButton>
+          </div>
         </div>
-      </div>
-    </Modal>
+      </Modal>
+    </div>
   );
 };
 
