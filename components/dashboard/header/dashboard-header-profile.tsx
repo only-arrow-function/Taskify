074ea98864f894acb6d99bd90b4b83c87fdfc8f2
d5ng <dong5ffice@gmail.com>
2024-04-23 15:07:59 +0900

    Feat: Zustand 로컬 스토리지 저장

--- components/dashboard/header/dashboard-header-profile.tsx
+++ components/dashboard/header/dashboard-header-profile.tsx
@@ -1,11 +1,17 @@
 import ProfileBadge from '@/components/profile/profile-badge';
 import ProfileUsername from '@/components/profile/profile-username';
+import { useAuthenticationStore } from '@/store/auth';
 
-const DashboardHeaderProfile = (props: { username: string }) => {
+const DashboardHeaderProfile = () => {
+  const nickname = useAuthenticationStore((state) => state.nickname);
+  const profileImage = useAuthenticationStore((state) => state.profileImageUrl);
+
+  console.log(nickname, profileImage);
+
   return (
     <div className="flex items-center gap-x-3">
-      <ProfileBadge>{props.username.slice(0, 1)}</ProfileBadge>
-      <ProfileUsername username={props.username} />
+      <ProfileBadge>{profileImage && nickname.slice(0, 1)}</ProfileBadge>
+      <ProfileUsername username={nickname} />
     </div>
   );
 };
