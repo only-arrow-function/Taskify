8322592e3d81f8f02d3412757d05f6bc73d848e7
ayoung-iya <rhaehfdl1879@naver.com>
2024-04-24 23:42:53 +0900

    refactor [윤아영] : profile 이미지 글자 들어갈 경우 대문자가 나오도록 변경

--- components/modal/todo/profile-image.tsx
+++ components/modal/todo/profile-image.tsx
@@ -6,10 +6,9 @@ const ProfileImage = ({ nickname, imageUrl }) => {
       {imageUrl && <Image src={imageUrl} alt="프로필 이미지" fill style={{ objectFit: 'cover' }} />}
       {nickname && (
         <span className="inline-flex justify-center items-center rounded-full text-xs font-[Montserrat] bg-[#A3C4A2] size-full text-white border-2 border-white">
-          {nickname.slice(0, 1)}
+          {nickname.slice(0, 1).toUpperCase()}
         </span>
       )}
-      
     </div>
   );
 };
