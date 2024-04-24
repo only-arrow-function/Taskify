12e12593d60613bd6e1d4e0fbf5c465fc0ade438
whai2 <whai2125@gmail.com>
2024-04-24 22:06:24 +0900

    refactor [노은수] : 사이드바, 대시보드 생성 모달, 대시보드 리스트의 서버 상태 리팩토링 (리액트 쿼리 적용), 대시 보드 생성과 컬러칩은 전역 상태로 분리

--- components/dashboard/list/dashboard-list-item.tsx
+++ components/dashboard/list/dashboard-list-item.tsx
@@ -1,19 +1,25 @@
 import Link from 'next/link';
+
 import DashboardListItemSkeleton from './dashboard-list-item-skeleton';
 import DashboardOpenButton from '@/components/buttons/domain/dashboard-open-button';
-import { useDashboards } from '@/hooks/swr/dashboard/use-dashboards';
+
+import { useInfiniteDashboardsQuery } from '@/hooks/react-query/use-query-dashboard';
 
 interface DashboardListItemProps {
   currentPage: number;
 }
 
 const DashboardListItem = (props: DashboardListItemProps) => {
-  const { data, isLoading } = useDashboards({ page: props.currentPage });
-  if (!data || isLoading) return <DashboardListItemSkeleton />;
+  const { data, isPending } = useInfiniteDashboardsQuery();
+
+  if (!data || isPending) return <DashboardListItemSkeleton />;
 
   return (
     <>
-      {data.dashboards.map((dashboard) => (
+      {data &&
+      data.pages[props.currentPage - 1] &&
+      data.pages[props.currentPage - 1].dashboards &&
+      data.pages[props.currentPage - 1].dashboards.map((dashboard) => (
         <li key={dashboard.id} className="relative">
           <Link href={`/dashboard/${dashboard.id}`}>
             <DashboardOpenButton color={dashboard.color}>{dashboard.title}</DashboardOpenButton>
