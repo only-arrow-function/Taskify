8fec1fbed64d009cb3374fceb5d98ccf8d70725e
whai2 <whai2125@gmail.com>
2024-04-24 21:12:11 +0900

    refactor [노은수] : 모달 전역 상태 활용, 사이드 바와 대시보드 추가 버튼을 누르면, 전역 상태의 모달 토글 상태를 관리하도록 적용

--- components/dashboard/list/dashboard-list.tsx
+++ components/dashboard/list/dashboard-list.tsx
@@ -10,9 +10,10 @@ const DashboardList = () => {
   const handleOpenToggle = useToggleStore((state) => state.handleOpenToggle);
   const [currentPage, setCurrentPage] = useState(1);
 
+  // 모달을 띄우는 부분은 사이드 바에서!
   return (
     <>
-      {isToggle && <NewDashboardModal />}
+      {/* {isToggle && <NewDashboardModal />} */}
       <section className="w-[80vw] my-6 ml-6 sm:w-[67vw] md:my-10 sm:ml-10 md:w-[70vw] xl:max-w-[1022px] lg:ml-10">
         <ul className="grid gap-[13px] md:grid-cols-2 lg:grid-cols-3">
           <li>
