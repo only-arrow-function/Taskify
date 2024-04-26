5387d26882efb711520bebf7642074fe79f3b338
whai2 <whai2125@gmail.com>
2024-04-26 17:37:03 +0900

    feat [노은수] : 카드 리액트 쿼리 적용

--- hooks/react-query/use-query-cards.ts
+++ hooks/react-query/use-query-cards.ts
@@ -1,3 +1,33 @@
 import { useInfiniteQuery, useMutation, QueryClient } from '@tanstack/react-query';
 
 import cardsRequests from "@/apis/cards-request";
+
+export const useInfiniteCardsQuery = (columnId: string) => {
+  const { data, isSuccess, isPending, hasNextPage, isFetchingNextPage, fetchNextPage } = useInfiniteQuery({
+    queryKey: [`${columnId}-cards`],
+    initialPageParam: 1,
+    queryFn: async ({ pageParam = 1 }) =>
+      await cardsRequests.fetchDashboards({ page: pageParam, navigationMethod: 'pagination' }),
+
+    getNextPageParam: (lastPage, allPages) => {
+      if (!lastPage) return undefined;
+
+      const totalPages = Math.ceil(lastPage.totalCount / PAGE_DASHBOARD_COUNT);
+      const nextPage = allPages.length + 1;
+
+      if (nextPage <= totalPages) {
+        return nextPage;
+      } else {
+        return undefined;
+      }
+    },
+
+    select: (data) => {
+      const totalCount = data.pages[0]?.totalCount || 0;
+      const totalPages = Math.ceil(totalCount / PAGE_DASHBOARD_COUNT);
+      return { totalPages: totalPages, totalCount, pages: data.pages };
+    },
+  });
+
+  return { data, isSuccess, isPending, hasNextPage, isFetchingNextPage, fetchNextPage };
+};
