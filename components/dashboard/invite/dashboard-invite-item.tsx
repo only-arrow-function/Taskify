6822453ee396d0e367062aa22f1d53f6d01e8042
ayoung-iya <rhaehfdl1879@naver.com>
2024-04-26 20:18:59 +0900

    feat [윤아영] : 초대받은 대시보드 테이블 api 연결

---
+++ components/dashboard/invite/dashboard-invite-item.tsx
@@ -0,0 +1,43 @@
+import { useQueryClient } from '@tanstack/react-query';
+import BasicButton from '@/components/buttons/basic-button';
+import { useAcceptInvitedDashboard, useRefuseInvitedDashboard } from '@/hooks/react-query/use-query-invited-dashboard';
+
+const DashboardInviteItem = ({
+  id,
+  dashboardTitle,
+  inviterName,
+}: {
+  id: number;
+  dashboardTitle: string;
+  inviterName: string;
+}) => {
+  const queryClient = useQueryClient();
+
+  const { mutate: refuseMutate } = useRefuseInvitedDashboard(id, queryClient);
+  const { mutate: acceptMutate } = useAcceptInvitedDashboard(id, queryClient);
+
+  return (
+    <li id={String(id)} className="md:flex items-center py-7 text-grayscale-80 border-b border-grayscale-30">
+      <div className="flex md:w-[36%]">
+        <span className="md:hidden text-grayscale-50">이름</span>
+        <span className="ml-7 md:ml-0">{dashboardTitle}</span>
+      </div>
+      <div className="flex sm:w-[30%] md:w-[30%] mt-2.5 md:mt-0">
+        <span className="md:hidden text-grayscale-50">초대자</span>
+        <span className="ml-4 md:ml-0">{inviterName}</span>
+      </div>
+      <div className="flex md:w-[34%] mt-4 md:mt-0">
+        <span className="flex gap-2.5">
+          <BasicButton purpose="positive" eventHandler={acceptMutate}>
+            수락
+          </BasicButton>
+          <BasicButton purpose="negative" eventHandler={refuseMutate}>
+            거절
+          </BasicButton>
+        </span>
+      </div>
+    </li>
+  );
+};
+
+export default DashboardInviteItem;
--- components/dashboard/invite/dashboard-invite.tsx
+++ components/dashboard/invite/dashboard-invite.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import DashboardInviteSearch from '@/components/dashboard/invite/dashboard-invite-search';
 import DashboardInviteTable from '@/components/dashboard/invite/dashboard-invite-table';
+import { useInvitedDashboard } from '@/hooks/react-query/use-query-invited-dashboard';
 
 const DashboardInvite = () => {
+  const { data } = useInvitedDashboard();
+
+  if (!data) return null;
+
   return (
     <section className="w-[80vw] h-[600px] ml-6 bg-white rounded-lg sm:w-[67vw] sm:ml-10 md:w-[70vw] xl:max-w-[1022px] lg:ml-10">
       <div className="px-4 py-6 sm:px-7 sm:pt-8">
         <h3 className="text-2xl font-bold">초대받은 대시보드</h3>
         <DashboardInviteSearch />
-        <DashboardInviteTable />
+        <DashboardInviteTable invitations={data.invitations} />
       </div>
     </section>
   );
