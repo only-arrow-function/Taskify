2f331fd1e104f65e2ff929570cf79fe591e4bcb4
whai2 <whai2125@gmail.com>
2024-04-29 14:38:55 +0900

    refactor [노은수] : 마이 페이지 수정

--- pages/mypage/index.tsx
+++ pages/mypage/index.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import AuthHOC from '@/auth/auth-hoc';
+import DashboardHeader from '@/components/dashboard/header/dashboard-header';
 import DashboardSectionLayout from '@/components/dashboard/layout/dashboard-section-layout';
-import MypageHeader from '@/components/mypage/header/header';
+//import MypageHeader from '@/components/mypage/header/header';
 import Management from '@/components/mypage/management/management';
 import SideMenu from '@/components/side-menu/side-menu';
 
 const Mypage = () => {
   return (
     <DashboardSectionLayout>
-      <MypageHeader />
-      <SideMenu />
-      <Management />
+      <SideMenu page={1}/>
+      <div className='flex-col w-full'>
+        <DashboardHeader />
+        <Management />
+      </div>
     </DashboardSectionLayout>
   );
 };
